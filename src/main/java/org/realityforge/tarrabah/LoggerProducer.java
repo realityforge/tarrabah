dca14f1c987aa13fb31f3e13e3f61e88484e456b
Peter Donald <peter@realityforge.org>
2013-04-07 20:03:27 +1000

    Add in a CDI producer for Logger

---
+++ src/main/java/org/realityforge/tarrabah/LoggerProducer.java
@@ -0,0 +1,17 @@
+package org.realityforge.tarrabah;
+
+import java.util.logging.Logger;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+
+/**
+ * Logging producer for injectable logger.
+ */
+public class LoggerProducer
+{
+  @Produces
+  public Logger produceLogger( final InjectionPoint injectionPoint )
+  {
+    return Logger.getLogger( injectionPoint.getMember().getDeclaringClass().getName() );
+  }
+}
