acbb6507699be158dfc22b79345785d773c5d661
Peter Donald <peter@realityforge.org>
2013-04-10 08:49:29 +1000

    Suppress warning

--- src/main/java/org/realityforge/tarrabah/LoggerProducer.java
+++ src/main/java/org/realityforge/tarrabah/LoggerProducer.java
@@ -7,6 +7,7 @@ import javax.enterprise.inject.spi.InjectionPoint;
 /**
  * Logging producer for injectable logger.
  */
+@SuppressWarnings( "UnusedDeclaration" )
 public class LoggerProducer
 {
   @Produces
