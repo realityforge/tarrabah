ccda756070f6a310259bc880fc965ae5de77e47b
Peter Donald <peter@realityforge.org>
2013-04-11 22:07:19 +1000

    Extract out superclass for UDP server

--- src/main/java/org/realityforge/tarrabah/SyslogUDPServer.java
+++ src/main/java/org/realityforge/tarrabah/SyslogUDPServer.java
@@ -1,78 +1,23 @@
 package org.realityforge.tarrabah;
 
-import java.net.InetSocketAddress;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.annotation.Nullable;
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import javax.inject.Inject;
-import org.jboss.netty.bootstrap.ConnectionlessBootstrap;
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.ChannelPipelineFactory;
 import org.jboss.netty.channel.Channels;
-import org.jboss.netty.channel.FixedReceiveBufferSizePredictorFactory;
-import org.jboss.netty.channel.socket.nio.NioDatagramChannelFactory;
 import org.realityforge.tarrabah.cdi.ext.pipeline.PipelineScoped;
 
 @PipelineScoped
 public class SyslogUDPServer
+  extends AbstractUDPServer
 {
-  @Inject
-  private Logger _logger;
-
-  private int _port = 8080;
-
-  @Nullable
-  private ConnectionlessBootstrap _bootstrap;
-  private ExecutorService _executorService;
-
-  @PostConstruct
-  public void postConstruct()
+  protected ChannelPipelineFactory newPipelineFactory()
   {
-    _executorService = Executors.newCachedThreadPool();
-    _bootstrap = new ConnectionlessBootstrap( new NioDatagramChannelFactory( _executorService ) );
-
-    _bootstrap.setOption( "receiveBufferSize", 1048576 );
-    _bootstrap.setOption( "receiveBufferSizePredictorFactory", new FixedReceiveBufferSizePredictorFactory( 1024 * 8 ) );
-
-    // Set up the pipeline factory.
-    _bootstrap.setPipelineFactory( new ChannelPipelineFactory()
+    return new ChannelPipelineFactory()
     {
       public ChannelPipeline getPipeline()
         throws Exception
       {
         return Channels.pipeline( new SyslogHandler() );
       }
-    } );
-
-    if ( _logger.isLoggable( Level.INFO ) )
-    {
-      _logger.info( "Binding UDP Server to port " + _port );
-    }
-    // Bind and start to accept incoming connections.
-    _bootstrap.bind( new InetSocketAddress( _port ) );
-  }
-
-  @PreDestroy
-  public void preDestroy()
-  {
-    if ( null != _bootstrap )
-    {
-      if ( _logger.isLoggable( Level.INFO ) )
-      {
-        _logger.info( "Shutting down UDP server on port " + _port );
-      }
-
-      _bootstrap.shutdown();
-      _bootstrap = null;
-    }
-    if ( null != _executorService )
-    {
-      _executorService.shutdown();
-      _executorService = null;
-    }
+    };
   }
 }
