6e6f24d2d71de064d52e2c3cc031fb4588a79f70
Peter Donald <peter@realityforge.org>
2013-04-14 08:13:07 +1000

    Rework example to try dynamic instantiation

--- src/main/java/org/realityforge/tarrabah/MainApp.java
+++ src/main/java/org/realityforge/tarrabah/MainApp.java
@@ -1,14 +1,25 @@
 package org.realityforge.tarrabah;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.InjectionTarget;
+import javax.inject.Inject;
 import org.apache.deltaspike.cdise.api.CdiContainer;
 import org.apache.deltaspike.cdise.api.CdiContainerLoader;
 import org.apache.deltaspike.cdise.api.ContextControl;
-import org.apache.deltaspike.core.api.provider.BeanProvider;
+import org.apache.deltaspike.core.api.provider.BeanManagerProvider;
 import org.realityforge.tarrabah.cdi.ext.pipeline.Pipeline;
 import org.realityforge.tarrabah.cdi.ext.pipeline.PipelineScoped;
 
 public class MainApp
 {
+  static class Foo
+  {
+    @Inject
+    private SyslogTCPServer _s;
+
+  }
   public static void main( final String[] args )
   {
     final CdiContainer cdiContainer = CdiContainerLoader.getCdiContainer();
@@ -16,32 +27,42 @@ public class MainApp
 
     // Starting the application-context allows to use @PipelineScoped beans
     final ContextControl contextControl = cdiContainer.getContextControl();
-    contextControl.startContext( PipelineScoped.class );
+    contextControl.startContext( ApplicationScoped.class );
+    //contextControl.startContext( PipelineScoped.class );
     contextControl.startContexts();
 
-    Pipeline.activate( new Pipeline( "foo" ) );
+    final Pipeline foo = new Pipeline( "foo" );
+    Pipeline.activate( foo );
 
-    final SyslogUDPServer udpServer = BeanProvider.getContextualReference( SyslogUDPServer.class );
-    final SyslogTCPServer tcpServer = BeanProvider.getContextualReference( SyslogTCPServer.class );
+    final BeanManager bm = BeanManagerProvider.getInstance().getBeanManager();
+    final CreationalContext<Foo> context = bm.createCreationalContext( null );
+    final InjectionTarget<Foo> injectionTarget = bm.createInjectionTarget( bm.createAnnotatedType( Foo.class ) );
+    final Foo instance = new Foo();
+    //final Foo instance = injectionTarget.produce( context );
+    injectionTarget.inject( instance, context );
+    injectionTarget.postConstruct( instance );
+    //final SyslogTCPServer tcpServer = BeanProvider.getContextualReference( SyslogTCPServer.class );
 
     System.err.println( "Active" );
 
-    /*
-    for ( int i = 0; i < 2; i++ )
-    {
-      try
-      {
-        Thread.sleep( 1000 );
-      }
-      catch ( final InterruptedException ie )
-      {
-        //ignored
-      }
-    }
-    */
+    //for ( int i = 0; i < 2; i++ )
+    //{
+    //  try
+    //  {
+    //    Thread.sleep( 1000 );
+    //  }
+    //  catch ( final InterruptedException ie )
+    //  {
+    //    //ignored
+    //  }
+    //}
 
     contextControl.stopContext( PipelineScoped.class );
     Pipeline.deactivate();
+
+    context.release();
+    //injectionTarget.preDestroy( instance );
+    //injectionTarget.dispose( instance );
 
     //contextControl.stopContexts();
     cdiContainer.shutdown();
