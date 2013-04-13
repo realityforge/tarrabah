8c035aac2f8144b8dc9e04aaa505641da26661f2
Peter Donald <peter@realityforge.org>
2013-04-13 11:11:10 +1000

    Introduce GelfChunkedMessage and add some basic tests around it

---
+++ src/main/java/org/realityforge/tarrabah/GelfChunkedMessage.java
@@ -0,0 +1,97 @@
+package org.realityforge.tarrabah;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/**
+ * Representation of a chunked Gelf message.
+ * <p/>
+ * This class contains all the received chunks, the last time a chunk was received etc.
+ */
+public class GelfChunkedMessage
+{
+  @Nonnull
+  private final Long _messageID;
+  @Nonnull
+  private final GelfMessageChunk[] _chunks;
+  private int _receivedChunksCount;
+  private long _lastUpdateTime;
+
+  public GelfChunkedMessage( @Nonnull final GelfMessageChunk initialChunk )
+  {
+    _messageID = initialChunk.getMessageID();
+    _chunks = new GelfMessageChunk[ initialChunk.getChunkCount() ];
+    _chunks[ initialChunk.getChunkSequence() ] = initialChunk;
+    _lastUpdateTime = currentTime();
+    _receivedChunksCount = 1;
+  }
+
+  @Nonnull
+  public Long getMessageID()
+  {
+    return _messageID;
+  }
+
+  public int getReceivedChunksCount()
+  {
+    return _receivedChunksCount;
+  }
+
+  public long getLastUpdateTime()
+  {
+    return _lastUpdateTime;
+  }
+
+  public void insert( final @Nonnull GelfMessageChunk chunk )
+  {
+    final int sequence = chunk.getChunkSequence();
+    if ( sequence >= _chunks.length )
+    {
+      throw new IllegalArgumentException( "Chunk sequence '" + sequence + "' exceeds " +
+                                          "expected chunk count '" + _chunks.length + "' " +
+                                          "for message '" + Long.toHexString( getMessageID() ) + "'" );
+    }
+    if ( chunk.getChunkCount() != _chunks.length )
+    {
+      throw new IllegalArgumentException( "Chunk count '" + chunk.getChunkCount() + "' does not match " +
+                                          "expected chunk count '" + _chunks.length + "' " +
+                                          "for message '" + Long.toHexString( getMessageID() ) + "'" );
+    }
+    if ( null != _chunks[ sequence ] )
+    {
+      throw new IllegalArgumentException( "Chunk sequence '" + sequence + "' already received " +
+                                          "for message '" + Long.toHexString( getMessageID() ) + "'" );
+    }
+    _chunks[ sequence ] = chunk;
+    _receivedChunksCount++;
+    _lastUpdateTime = currentTime();
+  }
+
+  public boolean isComplete()
+  {
+    return _receivedChunksCount == _chunks.length;
+  }
+
+  protected long currentTime()
+  {
+    return System.currentTimeMillis();
+  }
+
+  @Override
+  public String toString()
+  {
+    final StringBuilder sb = new StringBuilder();
+
+    sb.append( "GelfChunkedMessage[MessageID= " );
+    sb.append( getMessageID() );
+    sb.append( ",ReceivedChunksCount=" );
+    sb.append( _receivedChunksCount );
+    sb.append( "/" );
+    sb.append( _chunks.length );
+    sb.append( ",LastUpdate=" );
+    sb.append( _lastUpdateTime );
+    sb.append( "]" );
+
+    return sb.toString();
+  }
+}
---
+++ src/test/java/org/realityforge/tarrabah/GelfChunkedMessageTest.java
@@ -0,0 +1,101 @@
+package org.realityforge.tarrabah;
+
+import javax.annotation.Nonnull;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class GelfChunkedMessageTest
+{
+  private int _currentTime;
+
+  @BeforeMethod
+  public void resetTime()
+  {
+    _currentTime = 1;
+  }
+
+  @Test
+  public void basicWorkflow()
+    throws Exception
+  {
+    final GelfMessageChunk initialChunk = newChunk( 1, 0, 3, 'a' );
+    final GelfChunkedMessage message = new TestGelfChunkedMessage( initialChunk );
+
+    assertEquals( message.getReceivedChunksCount(), 1 );
+    assertEquals( message.isComplete(), false );
+    assertEquals( message.getLastUpdateTime(), 1 );
+    assertEquals( message.getMessageID(), initialChunk.getMessageID() );
+
+    message.insert( newChunk( 1, 1, 3, 'b' ) );
+
+    assertEquals( message.getReceivedChunksCount(), 2 );
+    assertEquals( message.isComplete(), false );
+    assertEquals( message.getLastUpdateTime(), 2 );
+    assertEquals( message.getMessageID(), initialChunk.getMessageID() );
+
+    message.insert( newChunk( 1, 2, 3, 'c' ) );
+
+    assertEquals( message.getReceivedChunksCount(), 3 );
+    assertEquals( message.isComplete(), true );
+    assertEquals( message.getLastUpdateTime(), 3 );
+  }
+
+  @Test( expectedExceptions = IllegalArgumentException.class,
+         expectedExceptionsMessageRegExp = "Chunk sequence '3' exceeds expected chunk count '3' for message '1'" )
+  public void invalidChunkSequence()
+    throws Exception
+  {
+    final GelfChunkedMessage message = new TestGelfChunkedMessage( newChunk( 1, 0, 3, 'a' ) );
+    message.insert( newChunk( 1, 3, 3, 'b' ) );
+  }
+
+  @Test( expectedExceptions = IllegalArgumentException.class,
+         expectedExceptionsMessageRegExp = "Chunk count '4' does not match expected chunk count '3' for message '1'" )
+  public void invalidChunkCount()
+    throws Exception
+  {
+    final GelfChunkedMessage message = new TestGelfChunkedMessage( newChunk( 1, 0, 3, 'a' ) );
+    message.insert( newChunk( 1, 2, 4, 'b' ) );
+  }
+
+  @Test( expectedExceptions = IllegalArgumentException.class,
+         expectedExceptionsMessageRegExp = "Chunk sequence '0' already received for message '1'" )
+  public void duplicateChunkMessage()
+    throws Exception
+  {
+    final GelfChunkedMessage message = new TestGelfChunkedMessage( newChunk( 1, 0, 3, 'a' ) );
+    message.insert( newChunk( 1, 0, 3, 'a' ) );
+  }
+
+  final class TestGelfChunkedMessage
+    extends GelfChunkedMessage
+  {
+    TestGelfChunkedMessage( @Nonnull final GelfMessageChunk initialChunk )
+    {
+      super( initialChunk );
+    }
+
+    @Override
+    protected long currentTime()
+    {
+      return _currentTime++;
+    }
+  }
+
+  private GelfMessageChunk newChunk( final int messageID,
+                                     final int chunkSequence,
+                                     final int chunkCount,
+                                     final char data )
+  {
+    final byte[] payload = new byte[]
+      {
+        (byte) GelfHandler.CHUNKED_BYTE_PREFIX[ 0 ],
+        (byte) GelfHandler.CHUNKED_BYTE_PREFIX[ 1 ],
+        (byte) messageID, 0, 0, 0, 0, 0, 0, 0, // Message ID 1
+        (byte) chunkSequence, (byte) chunkCount, //Chunk 1 of 2
+        (byte) data // Data
+      };
+    return new GelfMessageChunk( payload );
+  }
+}
