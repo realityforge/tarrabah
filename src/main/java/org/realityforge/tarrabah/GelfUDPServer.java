e11fa13bc4feaecb8e10d32cbe62707654ad6b56
Peter Donald <peter@realityforge.org>
2013-04-13 14:24:21 +1000

    Ensure handler is created via CDI

--- src/main/java/org/realityforge/tarrabah/GelfUDPServer.java
+++ src/main/java/org/realityforge/tarrabah/GelfUDPServer.java
@@ -1,5 +1,6 @@
 package org.realityforge.tarrabah;
 
+import javax.inject.Inject;
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.ChannelPipelineFactory;
 import org.jboss.netty.channel.Channels;
@@ -9,6 +10,9 @@ import org.realityforge.tarrabah.cdi.ext.pipeline.PipelineScoped;
 public class GelfUDPServer
   extends AbstractUDPServer
 {
+  @Inject
+  private GelfHandler _handler;
+
   protected ChannelPipelineFactory newPipelineFactory()
   {
     return new ChannelPipelineFactory()
@@ -16,7 +20,7 @@ public class GelfUDPServer
       public ChannelPipeline getPipeline()
         throws Exception
       {
-        return Channels.pipeline( new GelfHandler() );
+        return Channels.pipeline( _handler );
       }
     };
   }
